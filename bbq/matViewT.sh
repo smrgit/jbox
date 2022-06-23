946bf316d40b55818dadd9b4f1370049ac5e7390
Sheila M Reynolds <sheilamoorereynolds@gmail.com>
2022-06-23 15:54:13 +0000

    new variation on matViewTE.sh that does not run the "explore" part

---
+++ bbq/matViewT.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+p=$1
+d=$2
+v=$3
+
+t="$v""T"
+
+echo " "
+echo " in matView ... " $1  $2  $3 
+echo " "
+echo " "
+
+src="$p"."$d"."$v"
+echo $src
+
+dest="$p":"$d"."$t"
+echo $dest
+
+## -dry_run or nodry_run
+
+bq --quiet query --allow_large_results \
+        --destination_table=$dest \
+        --replace=true \
+        --nouse_legacy_sql \
+        --nodry_run \
+        "SELECT * FROM $src"
+
+
+## python3 $GIT_HOME/smrgit/jbox/bbq/bbqTableExplore.py -p $p -d $d -t $t
+
